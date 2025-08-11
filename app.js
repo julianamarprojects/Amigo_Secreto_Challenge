f3d823cde09bf239bd976cfb6f15a730e2f32316
julianamarprojects <julianamar@live.com>
2025-08-11 16:00:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+/* CHALLENGE AMIGO SECRETO 
+Se declara variable para poder almacenar los nombres del amigo secreto*/
+
+let amigos = []
+
+//Función para agregar amigos
+function agregarAmigo() {
+    let ingresarnombre = document.getElementById ("amigo").value.trim();
+
+    //Si el usuario no ingresa nada
+    if (ingresarnombre === "") {alert ("El campo no debe estar vacio, ingrese un nombre")
+        return;
+    }
+
+    //si el usuario ingresa un nombre duplicado
+    if (amigos.includes(ingresarnombre)){alert ("El nombre ya está en la lista.");
+        return;
+    }
+
+    //si cumple con las condiciones
+   else amigos.push (ingresarnombre);
+
+
+}
+
+
+
