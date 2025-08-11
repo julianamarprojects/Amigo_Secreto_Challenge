258da2a9f88fa9780890ccc4cb39838660492708
julianamarprojects <julianamar@live.com>
2025-08-11 16:56:58 -0500

    Update app.js
    Version funcional

--- app.js
+++ app.js
@@ -37,9 +37,27 @@ function actualizarListaAmigos() {
     lista.innerHTML = '';
 
     // un elemento li por cada amigo
-for(let i = 0; i < amigos.length; i++) {
-    const li = document.createElement('li');
-    li.textContent = amigos[i];
-    lista.appendChild(li);
+    for(let i = 0; i < amigos.length; i++) {
+        const li = document.createElement('li');
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
 }
+
+//funcion para sortear los amigos
+function sortearAmigo() {
+    //Verificar que haya al menos un elemento
+    if (amigos.length === 0) {
+        document.getElementById("resultado").textContent = "No hay amigos para el sorteo, agrega mínimo DOS";
+        return;
+    }
+    //Generar un índice aleatorio
+    let indicealeatorio = Math.floor(Math.random() * amigos.length);
+    
+    // Obtener el nombre sorteado
+    let nombreelegido = amigos[indicealeatorio];
+    console.log(nombreelegido);
+    
+    // Mostrar el amigo seleccionado
+    document.getElementById("resultado").textContent = `Amigo seleccionado: ${nombreelegido}`;
 }
